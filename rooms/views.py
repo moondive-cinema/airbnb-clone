110f9aaa49cd87089b6ba33981a3f1ade4ff3e42
moondive-cinema <moondive79@gmail.com>
2022-06-19 17:22:59 +0900

    view.py for home and room details

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+# from django.http import Http404
+# from django.shortcuts import render
+from . import models
+
+
+class HomeView(ListView):
+
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+
+
+class RoomDetail(DetailView):
+
+    """Room Detail Definition"""
+
+    model = models.Room
+
+
+"""
+fbv version
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/room_detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
+
+"""
