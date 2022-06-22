4b37699680b3c14ea8e5330f6ac384c21a5c9993
moondive-cinema <moondive79@gmail.com>
2022-06-22 22:36:56 +0900

    serchview with django forms. still need more tests if everything is ok.

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,10 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 
 # from django.http import Http404
-from django_countries import countries
+# from django_countries import countries
 from django.shortcuts import render
-from . import models
+from django.core.paginator import Paginator
+from . import models, forms
 
 
 class HomeView(ListView):
@@ -23,6 +24,85 @@ class RoomDetail(DetailView):
     model = models.Room
 
 
+class SearchView(View):
+
+    """Search View Definition"""
+
+    def get(self, request):
+
+        country = request.GET.get("country")
+
+        if country:
+            form = forms.SearchForm(request.GET)
+
+            if form.is_valid():
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
+
+                filter_args = {}
+
+                if city != "Anywhere":
+                    filter_args["city__contains"] = city
+
+                filter_args["country"] = country
+
+                if room_type is not None:
+                    filter_args["room_type"] = room_type
+
+                if price is not None:
+                    filter_args["price__lte"] = price
+
+                if guests is not None:
+                    filter_args["guests__gte"] = guests
+
+                if bedrooms is not None:
+                    filter_args["bedrooms__gte"] = bedrooms
+
+                if beds is not None:
+                    filter_args["beds__gte"] = beds
+
+                if baths is not None:
+                    filter_args["baths__gte"] = baths
+
+                if instant_book is True:
+                    filter_args["instant_book"] = True
+
+                if superhost is True:
+                    filter_args["superhost"] = True
+
+                if amenities is not None:
+                    for amenity in amenities:
+                        filter_args["amenities"] = amenity
+
+                if facilities is not None:
+                    for facility in facilities:
+                        filter_args["facilities"] = facility
+
+                qs = models.Room.objects.filter(**filter_args).order_by("-created")
+                paginator = Paginator(qs, 10, orphans=5)
+                page = request.GET.get("page", 1)
+                rooms = paginator.get_page(page)
+
+                return render(
+                    request, "rooms/search.html", {"form": form, "rooms": rooms}
+                )
+
+        else:
+            form = forms.SearchForm()
+
+        return render(request, "rooms/search.html", {"form": form})
+
+
 """
 # fbv version room detail
 
@@ -33,8 +113,8 @@ def room_detail(request, pk):
     except models.Room.DoesNotExist:
         raise Http404()
 
-"""
 
+# search function building from the scratch version
 
 def search(request):
     city = request.GET.get("city", "Anywhere")
@@ -123,3 +203,4 @@ def search(request):
         "rooms/search.html",
         {**form, **choices, "rooms": rooms},
     )
+"""
