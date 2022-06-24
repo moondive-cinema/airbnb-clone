2bac58876b9e578f7676dedf2b5e6cebd09f8138
moondive-cinema <moondive79@gmail.com>
2022-06-25 00:09:55 +0900

    user login and logout

--- users/models.py
+++ users/models.py
@@ -26,6 +26,15 @@ class User(AbstractUser):
 
     CURRENCY_CHOICES = ((CURRENCY_KRW, "KRW"), (CURRENCY_USD, "USD"))
 
+    """
+    유저네임을 이메일로 받기 위한 코드 스니펫. 
+    검증 필요. 빠르게 테스트 해봤을 때는 작동하지 않았음.
+
+    USERNAME_FIELD = "email"
+    email = models.EmailField(('email address'), unique=True)
+    REQUIRED_FIELDS = []
+    """
+
     avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     birthdate = models.DateField(blank=True, null=True)
